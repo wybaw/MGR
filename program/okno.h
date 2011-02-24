525c74380b3d6f4d12a9f9b0b344339c88e734d6
wybaw <michalwybawinski@gmail.com>
2011-02-24 14:57:48 +0100

    first commit

---
+++ program/okno.h
@@ -0,0 +1,53 @@
+#ifndef OKNO_H__
+#define OKNO_H__
+
+#include <QWidget>
+
+#include "ui_storczyk.h"
+
+#define QT_NO_DEBUG_OUTPUT
+
+class QWidget;
+class QLablel;
+class HoverPoints;
+
+class Okno: public QMainWindow, Ui::Okno
+{
+	Q_OBJECT
+
+public:
+	Okno(QWidget * parent);
+
+protected:
+	void paintEvent ( QPaintEvent * event );
+
+private slots:
+	void on_actionOProgramie_triggered(bool);
+	void on_actionOQt_triggered(bool);
+	void on_actionOtworzZdjecie_triggered(bool);
+	void on_actionZmniejsz_triggered(bool);
+	void on_actionPowieksz_triggered(bool);
+	void on_actionOryginalny_triggered(bool);
+
+//	void on_pushObrysNerwu_toggled(bool);
+
+	void zmianaPunktow(double nerw, double brzeg);
+	void gotowyDoEdycji(bool stan);
+
+signals:
+	void reset();
+
+private:
+	void _skalujObrazek(const double x);
+	void _dostosujPasekPrzewijania(QScrollBar* scrollBar, double x);
+	void _ustawSkale(double x);
+
+private:
+	double _skala;
+	QLabel* _zdjecie;
+	HoverPoints* _hoverPoints;
+	double _nerw;
+	double _brzeg;
+};
+#endif
+
---
+++ work/P-build-desktop/ui_mainwindow.h
@@ -0,0 +1,99 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created: Wed Feb 23 18:47:49 2011
+**      by: Qt User Interface Compiler version 4.7.0
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtGui/QAction>
+#include <QtGui/QApplication>
+#include <QtGui/QButtonGroup>
+#include <QtGui/QHeaderView>
+#include <QtGui/QMainWindow>
+#include <QtGui/QMenuBar>
+#include <QtGui/QPushButton>
+#include <QtGui/QScrollArea>
+#include <QtGui/QScrollBar>
+#include <QtGui/QStatusBar>
+#include <QtGui/QToolBar>
+#include <QtGui/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralWidget;
+    QPushButton *pushButton;
+    QScrollArea *scrollArea;
+    QWidget *scrollAreaWidgetContents;
+    QScrollBar *horizontalScrollBar;
+    QScrollBar *verticalScrollBar;
+    QMenuBar *menuBar;
+    QToolBar *mainToolBar;
+    QStatusBar *statusBar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
+        MainWindow->resize(400, 300);
+        centralWidget = new QWidget(MainWindow);
+        centralWidget->setObjectName(QString::fromUtf8("centralWidget"));
+        pushButton = new QPushButton(centralWidget);
+        pushButton->setObjectName(QString::fromUtf8("pushButton"));
+        pushButton->setGeometry(QRect(300, 190, 75, 23));
+        scrollArea = new QScrollArea(centralWidget);
+        scrollArea->setObjectName(QString::fromUtf8("scrollArea"));
+        scrollArea->setGeometry(QRect(20, 20, 261, 221));
+        scrollArea->setWidgetResizable(true);
+        scrollAreaWidgetContents = new QWidget();
+        scrollAreaWidgetContents->setObjectName(QString::fromUtf8("scrollAreaWidgetContents"));
+        scrollAreaWidgetContents->setGeometry(QRect(0, 0, 257, 217));
+        horizontalScrollBar = new QScrollBar(scrollAreaWidgetContents);
+        horizontalScrollBar->setObjectName(QString::fromUtf8("horizontalScrollBar"));
+        horizontalScrollBar->setGeometry(QRect(-1, 200, 261, 20));
+        horizontalScrollBar->setOrientation(Qt::Horizontal);
+        verticalScrollBar = new QScrollBar(scrollAreaWidgetContents);
+        verticalScrollBar->setObjectName(QString::fromUtf8("verticalScrollBar"));
+        verticalScrollBar->setGeometry(QRect(240, 0, 16, 201));
+        verticalScrollBar->setOrientation(Qt::Vertical);
+        scrollArea->setWidget(scrollAreaWidgetContents);
+        MainWindow->setCentralWidget(centralWidget);
+        menuBar = new QMenuBar(MainWindow);
+        menuBar->setObjectName(QString::fromUtf8("menuBar"));
+        menuBar->setGeometry(QRect(0, 0, 400, 18));
+        MainWindow->setMenuBar(menuBar);
+        mainToolBar = new QToolBar(MainWindow);
+        mainToolBar->setObjectName(QString::fromUtf8("mainToolBar"));
+        MainWindow->addToolBar(Qt::TopToolBarArea, mainToolBar);
+        statusBar = new QStatusBar(MainWindow);
+        statusBar->setObjectName(QString::fromUtf8("statusBar"));
+        MainWindow->setStatusBar(statusBar);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", 0, QApplication::UnicodeUTF8));
+        pushButton->setText(QApplication::translate("MainWindow", "PushButton", 0, QApplication::UnicodeUTF8));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
