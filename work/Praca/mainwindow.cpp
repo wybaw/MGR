c9611af14bfed8684d17665bbc181f76c0e63e63
wybaw <michalwybawinski@gmail.com>
2011-02-27 18:35:59 +0100

    Printing added

--- work/Praca/mainwindow.cpp
+++ work/Praca/mainwindow.cpp
@@ -5,54 +5,72 @@ MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
+    //initial setup for GUI elements
     ui->setupUi(this);
     setWindowTitle(tr("CellViewer"));
     ui->toolBar->addAction(ui->actionOpen);
-
-
+    //main label ,where graphic files are shown
     label = new QLabel;
     label->setBackgroundRole(QPalette::Base);    
     ui->scrollArea->setWidget(label);
+
 
 
     connect(ui->actionOpen,SIGNAL(triggered()),this,SLOT(openFile()));
     connect(ui->actionClose,SIGNAL(triggered()),qApp,SLOT(quit()));
     connect(ui->actionAbout_Qt,SIGNAL(triggered()),qApp,SLOT(aboutQt()));
     connect(ui->ScreenshotButton,SIGNAL(clicked()),this,SLOT(takeScreenshot()));
+    connect(ui->actionPrint,SIGNAL(triggered()),this,SLOT(print()));
 
 }
-void MainWindow::takeScreenshot()
+void MainWindow::takeScreenshot()   // TODO - block SLOT until file will be loaded
 {
-    screenshot = label->pixmap();
-
     if(screenshot)
     {
-        screenshot->save("screenshot222.png","PNG");
-    }
+        screenshot->save("screenshot222.png","PNG");    // TODO name of screenshot should be created
+    }                                                   // based on File name.
     else
     {
         qFatal("Failure in taking screenshot");
     }
 }
+
+void MainWindow::print()    // TODO - block SLOT until file will be loaded
+{
+    QPrinter printer(QPrinter::HighResolution);
+    printer.setColorMode(QPrinter::Color);
+    printer.setOrientation(QPrinter::Landscape);
+
+    QPrintDialog dialog(&printer,this);
+
+    if(dialog.exec() == QDialog::Accepted)
+    {
+        printer.newPage();
+        QPainter p(&printer);
+        QPixmap resized = screenshot->scaledToWidth(printer.pageRect().width());
+        p.drawPixmap(0,0,resized);
+        p.end();
+    }
+}
+
 MainWindow::~MainWindow()
 {
     delete ui;
 }
 void MainWindow::openFile()
 {
-    QString fileName = QFileDialog::getOpenFileName(this,
-                       tr("Open File"), QDir::currentPath());
+    fileName = QFileDialog::getOpenFileName(this,tr("Open File"),"../../komorki-zdjecia");
     if (!fileName.isEmpty())
     {
-        QPixmap image(fileName);
-        if (image.isNull())
+        image = new QPixmap(fileName);
+        if (image->isNull())
         {
-            QMessageBox::information(this, tr("CellViewer"),
-                                     tr("Nie mogê za³adowaæ pliku \'%1\'.").arg(fileName));
+            QMessageBox::information(this, tr("CellViewer"),tr("Nie mogê za³adowaæ pliku \'%1\'.").arg(fileName));
             return;
         }
 
-        label->setPixmap(image);
-        //label->adjustSize();
+        label->setPixmap(*image);
+        //copy of image ,for printing and making screenshot
+        screenshot = label->pixmap();
     }
 }
--- work/Praca/mainwindow.h
+++ work/Praca/mainwindow.h
@@ -7,6 +7,14 @@
 #include <QImage>
 #include <QFileDialog>
 #include <QMessageBox>
+#include <QPrinter>
+#include <QPrintDialog>
+#include <QPainter>
+#include <QPlastiqueStyle>
+
+
+
+
 namespace Ui {
     class MainWindow;
 }
@@ -21,11 +29,13 @@ public:
 public slots:
     void openFile();
     void takeScreenshot();
-
+    void print();
 private:
     Ui::MainWindow *ui;
     QLabel * label;
     const QPixmap* screenshot;
+    QPixmap* image;
+    QString fileName;
 };
 
 #endif // MAINWINDOW_H
