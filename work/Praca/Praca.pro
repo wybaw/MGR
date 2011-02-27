108db9bd57e8254a81485de06bd1056a45010242
wybaw <michalwybawinski@gmail.com>
2011-02-27 11:48:20 +0100

    Praca magisterska

---
+++ work/Praca/Praca.pro
@@ -0,0 +1,18 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2011-02-27T11:43:28
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+TARGET = Praca
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp
+
+HEADERS  += mainwindow.h
+
+FORMS    += mainwindow.ui
---
+++ work/Praca/mainwindow.cpp
@@ -0,0 +1,46 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    setWindowTitle(tr("CellViewer"));
+    ui->toolBar->addAction(ui->actionOpen);
+
+
+    label = new QLabel;
+    label->setBackgroundRole(QPalette::Base);    
+    ui->scrollArea->setWidget(label);
+
+
+    connect(ui->actionOpen,SIGNAL(triggered()),this,SLOT(openFile()));
+    connect(ui->actionClose,SIGNAL(triggered()),qApp,SLOT(quit()));
+    connect(ui->actionAbout_Qt,SIGNAL(triggered()),qApp,SLOT(aboutQt()));
+
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+void MainWindow::openFile()
+{
+    QString fileName = QFileDialog::getOpenFileName(this,
+                       tr("Open File"), QDir::currentPath());
+    if (!fileName.isEmpty())
+    {
+        QPixmap image(fileName);
+        if (image.isNull())
+        {
+            QMessageBox::information(this, tr("CellViewer"),
+                                     tr("Nie mogê za³adowaæ pliku \'%1\'.").arg(fileName));
+            return;
+        }
+
+        label->setPixmap(image);
+        //label->adjustSize();
+    }
+}
